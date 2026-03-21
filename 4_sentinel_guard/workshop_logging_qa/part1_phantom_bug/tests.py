f6a41c124595eaf3ce67c6bcd627a64965b23789
suphatsorn <suphatsorn@storemesh.com>
2026-03-21 13:15:40 +0700

    [ADD] test.py

--- 4_sentinel_guard/workshop_logging_qa/part1_phantom_bug/tests.py
+++ 4_sentinel_guard/workshop_logging_qa/part1_phantom_bug/tests.py
@@ -1,24 +1,36 @@
 from unittest import TestCase
 from .models import Wallet
 
+
 class WalletTransferTest(TestCase):
     def setUp(self):
-        # Arrange: เตรียมข้อมูลเบื้องต้นก่อนเริ่มเทสต์
         self.wallet_a = Wallet(user_id="A001", balance=100)
         self.wallet_b = Wallet(user_id="B002", balance=50)
 
     def test_transfer_fails_if_insufficient_balance(self):
-        # TODO Mission 1: เขียน Unit Test ตามโจทย์
-        pass
+        with self.assertRaises(ValueError) as context:
+            self.wallet_a.transfer(self.wallet_b, 500)
+        self.assertEqual(str(context.exception), "Insufficient balance")
+        self.assertEqual(self.wallet_a.balance, 100)
+        self.assertEqual(self.wallet_b.balance, 50)
 
     def test_transfer_success_happy_path(self):
-        # TODO Mission 1: เขียน Unit Test ตามโจทย
-        pass
+        result = self.wallet_a.transfer(self.wallet_b, 40)
+
+        self.assertTrue(result)
+        self.assertEqual(self.wallet_a.balance, 60)
+        self.assertEqual(self.wallet_b.balance, 90)
 
     def test_transfer_exact_balance_success(self):
-        # TODO Mission 1: เขียน Unit Test ตามโจทย์
-        pass
+        result = self.wallet_a.transfer(self.wallet_b, 100)
+
+        self.assertTrue(result)
+        self.assertEqual(self.wallet_a.balance, 0)
+        self.assertEqual(self.wallet_b.balance, 150)
 
     def test_transfer_fails_if_negative_amount(self):
-        # TODO Mission 1: เขียน Unit Test ตามโจทย์
-        pass
+        with self.assertRaises(ValueError) as context:
+            self.wallet_a.transfer(self.wallet_b, -10)
+
+        self.assertEqual(self.wallet_a.balance, 100)
+        self.assertEqual(self.wallet_b.balance, 50)
